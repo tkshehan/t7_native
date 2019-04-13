43489343f0e61e3474aaad4a02395de46ece6b2b
Thomas Shehan <thomas.shehan88@gmail.com>
2019-04-13 15:37:19 -0400

    add initial/test data

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'T7 Frames',
       theme: ThemeData(
         // This is the theme of your application.
         //
