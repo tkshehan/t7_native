18a8285364803688e5302757585460c44d359b68
Thomas Shehan <thomas.shehan88@gmail.com>
2019-04-26 11:54:59 -0400

    load and save data from api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async' show Future;
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 import 'FighterSelect.dart';
@@ -21,8 +23,18 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    _loadJsonAsset().then((result) {
-      Map data = json.decode(result);
+    _fetchApiData();
+    _loadData();
+  }
+
+  void _loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonData = prefs.getString('data') ?? 0;
+
+    if (jsonData == 0) {
+      _loadInitalData();
+    } else {
+      Map data = json.decode(jsonData);
       List keys = List.from(data.keys);
       keys.sort();
       setState(() {
@@ -30,11 +42,51 @@ class _MyAppState extends State<MyApp> {
         fighters = keys;
         loaded = true;
       });
-    });
+    }
   }
 
-  Future<String> _loadJsonAsset() async {
-    return await rootBundle.loadString('assets/data/fighters.json');
+  void _saveData(json) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('data', json);
+  }
+
+  void _loadInitalData() {
+    Future<String> _loadJsonAsset() async {
+      return await rootBundle.loadString('assets/data/fighters.json');
+    }
+
+    _loadJsonAsset()
+      ..then((result) {
+        Map data = json.decode(result);
+        List keys = List.from(data.keys);
+        keys.sort();
+        setState(() {
+          this.data = data;
+          fighters = keys;
+          loaded = true;
+        });
+      });
+  }
+
+  void _fetchApiData() {
+    Future<http.Response> _fetchData() {
+      return http.get('https://t7frames-server.herokuapp.com/frame-data');
+    }
+
+    _fetchData()
+      ..then((res) {
+        if (res.statusCode != 200) return;
+
+        Map data = json.decode(res.body);
+        List keys = List.from(data.keys);
+        keys.sort();
+        setState(() {
+          this.data = data;
+          fighters = keys;
+        });
+
+        _saveData(res.body);
+      });
   }
 
   @override
